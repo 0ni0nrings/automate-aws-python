c009105e990088efab7daba6e482082396543f0a
EC2 Default User <ec2-user@ip-172-31-69-71.ec2.internal>
2022-01-27 09:42:20 +0000

    broke script into functions

--- ec2.py
+++ ec2.py
@@ -2,44 +2,54 @@ import boto3
 
 DRYRUN = False
 
-ec2_client = boto3.client('ec2')
-images = ec2_client.describe_images(
-    Filters=[
-        {
-            'Name': 'name',
-            'Values': [
-                'amzn2-ami-hvm*',
-            ]
-        },
-        {
-            'Name': 'owner-alias',
-            'Values': [
-                'amazon',
-            ]
-        }
-    ]    
-)
-ec2_image = boto3.resource('ec2')
-AMI = ec2_image.Image(images['Images'][0]['ImageId'])
-# imageId = (images['Images'][0]['ImageId'])
-if AMI.state == 'available':
-    print(AMI.image_id)
-    # print(imageId)
-    instance = ec2_client.run_instances(
-        # ImageId=imageId,
-        ImageId=AMI.image_id,
-        InstanceType='t2.micro',
-        MaxCount=1,
-        MinCount=1,
-        DryRun=DRYRUN
+def Get_Image(ec2_client):
+    
+    images = ec2_client.describe_images(
+        Filters=[
+            {
+                'Name': 'name',
+                'Values': [
+                    'amzn2-ami-hvm*',
+                ]
+            },
+            {
+                'Name': 'owner-alias',
+                'Values': [
+                    'amazon',
+                ]
+            }
+        ]    
     )
-    ec2_instance = boto3.resource('ec2')
-    ec2 = ec2_instance.Instance(instance['Instances'][0]['InstanceId'])
+    ec2_image = boto3.resource('ec2')
+    AMI = ec2_image.Image(images['Images'][0]['ImageId'])
+    return AMI
+
+def Start_Ec2(AMI, ec2_client):
+    if AMI.state == 'available':
+        print(AMI.image_id)
+        # print(imageId)
+        instance = ec2_client.run_instances(
+            # ImageId=imageId,
+            ImageId=AMI.image_id,
+            InstanceType='t2.micro',
+            MaxCount=1,
+            MinCount=1,
+            DryRun=DRYRUN
+        )
+        ec2_instance = boto3.resource('ec2')
+        ec2 = ec2_instance.Instance(instance['Instances'][0]['InstanceId'])
+        return ec2
+    else:
+        print("AMI not available")
+        return None
+    
+if __name__ == '__main__':
+    ec2_client = boto3.client('ec2')
+    AMI = Get_Image(ec2_client)
+    ec2 = Start_Ec2(AMI=AMI, ec2_client=ec2_client)
     print(ec2.instance_id)
     ec2.wait_until_running()
-    print(f"Instance is {ec2.state['Name']} is spinning up")
+    print(f"Instance is {ec2.state['Name']}")
     ec2.terminate()
     ec2.wait_until_running()
-    print(f"Instance is {ec2.state['Name']} is terminating")
-else:
-    print("AMI not available")
+    print(f"Instance is {ec2.state['Name']}")
