2ec3d74f1b0edc00c47bc4b31f33a2c53f5eecf7
EC2 Default User <ec2-user@ip-172-31-69-71.ec2.internal>
2022-01-27 09:00:44 +0000

    using boto3 resource

--- ec2.py
+++ ec2.py
@@ -1,6 +1,6 @@
 import boto3
 
-DRYRUN = False
+DRYRUN = True
 
 ec2_client = boto3.client('ec2')
 images = ec2_client.describe_images(
@@ -19,13 +19,22 @@ images = ec2_client.describe_images(
         }
     ]    
 )
-imageId = (images['Images'][0]['ImageId'])
-print(imageId)
-instance = ec2_client.run_instances(
-    ImageId=imageId,
-    InstanceType='t2.micro',
-    MaxCount=1,
-    MinCount=1,
-    DryRun=DRYRUN
-)
-print(instance)
+ec2_image = boto3.resource('ec2')
+AMI = ec2_image.Image(images['Images'][0]['ImageId'])
+# imageId = (images['Images'][0]['ImageId'])
+if AMI.state == 'available':
+    print(AMI.image_id)
+    # print(imageId)
+    instance = ec2_client.run_instances(
+        # ImageId=imageId,
+        ImageId=AMI.image_id,
+        InstanceType='t2.micro',
+        MaxCount=1,
+        MinCount=1,
+        DryRun=DRYRUN
+    )
+    ec2_instance = boto3.resource('ec2')
+    ec2 = ec2_instance.Instance(instance['instances'][0]['InstanceId'])
+    print(ec2.instance_id)
+else:
+    print("AMI not available")
